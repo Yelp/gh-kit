aad045d0a125875f9b4a8dabcfb89144458e7a4d
Ben Asher <basher@yelp.com>
2013-10-18 11:52:14 -0700

    moved comment inside autorelease block in TestsFramework/main.m

--- TestsFramework/main.m
+++ TestsFramework/main.m
@@ -57,9 +57,9 @@ int main(int argc, char *argv[]) {
    */
   NSSetUncaughtExceptionHandler(&exceptionHandler);
   
-  // Register any special test case classes
-  //[[GHTesting sharedInstance] registerClassName:@"GHSpecialTestCase"];
   @autoreleasepool {
+    // Register any special test case classes
+    //[[GHTesting sharedInstance] registerClassName:@"GHSpecialTestCase"];
     int retVal = 0;
     // If GHUNIT_CLI is set we are using the command line interface and run the tests
     // Otherwise load the GUI app
