0ba0333725fa6e1859fab1e6e905f350ce1b38ec
Ben Asher <basher@yelp.com>
2014-01-08 12:29:36 -0800

    removed unused error variable

--- Classes/GHValidators.m
+++ Classes/GHValidators.m
@@ -34,8 +34,7 @@
 + (BOOL)isEmailAddress:(NSString *)str {
   // Simple regex from http://www.regular-expressions.info/email.html
   NSString *emailRegexPattern = @"^[A-Z0-9._%+\\-\\'\"]+@[A-Z0-9.-]+\\.[A-Z]+$";
-  NSError *error = nil;
-  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:emailRegexPattern options:NSRegularExpressionCaseInsensitive error:&error];
+  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:emailRegexPattern options:NSRegularExpressionCaseInsensitive error:nil];
   return ([regex numberOfMatchesInString:str options:0 range:NSMakeRange(0, [str length])] == 1);
 }
 
